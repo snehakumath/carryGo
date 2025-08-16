dd564acf6356585957d0867880f97b6dddd61811
Sneha Kumath <snehakumath@gmail.com>
2025-08-16 14:54:15 +0530

    Admin

--- Frontend/src/admin/AdminDashboard.jsx
+++ Frontend/src/admin/AdminDashboard.jsx
@@ -14,7 +14,7 @@ const AdminDashboard = () => {
       <main className="ml-64 p-6 w-full bg-gray-100 min-h-screen">
         <Routes>
         <Route index element={<AdminHome />} />
-          {/* <Route path="/" element={<AdminHome />} />  */}
+           <Route path="/" element={<AdminHome />} /> 
           <Route path="/customers" element={<Customers />} />
           <Route path="/transporters" element={<Transporters />} />
           <Route path="/queries" element={<Queries />} />
